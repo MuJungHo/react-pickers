16bb0dfd7a88792d62fbc5fe7e17de29f12af0ad
Ron Ho <ande1218@gmail.com>
2021-11-25 22:15:01 +0800

    [更新] 時間選擇器時針秒針

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,91 +1,93 @@
 import React from 'react'
-const Pointer = ({ point, hour, setHour, d }) => {
-  const isSelectedHour = (point.value % 12) === (hour % 12)
+const Pointer = ({ point, value, setValue, d, isHour }) => {
+  const isSelected = point.value === value
   const [isHover, setHover] = React.useState(false)
   return (
     <>
-      <line x1={point.x} y1={point.y} x2={d} y2={d} strokeWidth="3" stroke={isHover || isSelectedHour ? "#fff" : "#bebebe"} />
       <circle
-        onClick={() => setHour(point.value)}
+        onClick={() => setValue(isHour ? point.value : point.value * 5)}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
         cx={point.x}
         cy={point.y}
-        r={isHover || isSelectedHour ? 15 : 10}
-        fill={isHover || isSelectedHour ? "#fff" : "#f5f5f5"}
+        r={20}
+        fill={isHover || isSelected ? "#fff" : "#f5f5f5"}
       />
       <text
-        onClick={() => setHour(point.value)}
+        onClick={() => setValue(isHour ? point.value : point.value * 5)}
         onMouseEnter={() => setHover(true)}
-        x={point.x}
-        y={point.y}
-        stroke={isHover || isSelectedHour ? "#000" : "#000"}>{point.value + 1}</text>
+        x={
+          isHour ?
+            point.value > 9
+              ? point.x - 9
+              : point.x - 5
+            :
+            point.value > 1
+              ? point.x - 9
+              : point.x - 5
+        }
+        y={point.y + 5}
+        stroke={"#000"}>
+        {
+          isHour
+            ? point.value === 0 ? 12 : point.value
+            : point.value * 5
+        }
+      </text>
     </>
   )
 }
 const TimePicker = () => {
   const dt = new Date();
-  const [hour, setHour] = React.useState(dt.getHours())
+  const [hour, setHour] = React.useState(dt.getHours() % 12)
+  const [minute, setMinute] = React.useState(dt.getMinutes())
+  const [isPickHour, setPickHour] = React.useState(true)
+  const [isAM, setAM] = React.useState(dt.getHours() < 12)
   const d = 200
-  const hourPoints = [
-    { value: 0, x: 275, y: 75 },
-    { value: 1, x: 325, y: 125 },
-    { value: 2, x: 350, y: d },
-    { value: 3, x: 325, y: 275 },
-    { value: 4, x: 275, y: 325 },
-    { value: 5, x: d, y: 350 },
-    { value: 6, x: 125, y: 325 },
-    { value: 7, x: 75, y: 275 },
-    { value: 8, x: 50, y: d },
-    { value: 9, x: 75, y: 125 },
-    { value: 10, x: 125, y: 75 },
-    { value: 11, x: d, y: 50 }
-  ]
-
-  const hourPoints_ = [...Array(12).keys()].map(index => {
+  const points = [...Array(12).keys()].map(index => {
+    var hudu = (2 * Math.PI / 360) * 30 * index;
+    var x = 200 + Math.sin(hudu) * 150;
+    var y = 200 - Math.cos(hudu) * 150;
+    var hx = 200 + Math.sin(hudu) * 100;
+    var hy = 200 - Math.cos(hudu) * 100;
+    var mx = 200 + Math.sin(hudu) * 120;
+    var my = 200 - Math.cos(hudu) * 120;
     return ({
       value: index,
-      x: 275 + 150 * Math.sin(index * 30),
-      y: 75 - 150 * Math.sin(index * 30),
+      x,
+      y,
+      hx,
+      hy,
+      mx,
+      my
     })
   })
-  console.log(hourPoints_)
-  const minutePoints = [...Array(48).keys()].map(index => {
-
-  })
   return (
-    <div>
+    <div style={{ width: 540 }}>
       <p>Time Picker</p>
-      <div>{`${hour + 1}:${0}`}</div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>{`${isAM ? 'AM' : 'PM'} ${hour}:${minute}`}</div>
+        <div onClick={() => setPickHour(!isPickHour)}>{isPickHour ? 'To Pick Minute' : 'To Pick Hour'}</div>
+      </div>
       <svg
         height="400"
         width="400"
       >
         <circle cx={d} cy={d} r="180" stroke="#bebebe" strokeWidth="3" fill="#bebebe" />
         {
-          hourPoints_.map(point => <Pointer
+          points.map(point => <Pointer
             key={point.value}
             point={point}
-            hour={hour}
-            setHour={setHour}
+            value={isPickHour ? hour % 12 : Math.floor(minute / 5)}
+            setValue={isPickHour ? setHour : setMinute}
             d={d}
+            isHour={isPickHour}
           />)
         }
-      </svg>
-      <svg
-        height="400"
-        width="400"
-      >
-        <circle cx={d} cy={d} r="180" stroke="#bebebe" strokeWidth="3" fill="#bebebe" />
-        {
-          hourPoints.map(point => <Pointer
-            key={point.value}
-            point={point}
-            hour={hour}
-            setHour={setHour}
-            d={d}
-          />)
-        }
+        <line x1={points[hour % 12].hx} y1={points[hour % 12].hy} x2={d} y2={d} strokeWidth="3" stroke={isPickHour ? "orange" : "#f5f5f5"} />
+        <line x1={points[Math.floor(minute / 5)].mx} y1={points[Math.floor(minute / 5)].my} x2={d} y2={d} strokeWidth="3" stroke={isPickHour ? "#f5f5f5" : "orange"} />
+        <circle cx={d} cy={d} r="10" fill="#f5f5f5" />
+
       </svg>
     </div>
   )
