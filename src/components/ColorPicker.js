2b4fdcf074b69607fdb1c61412a863586662b69d
Ron Ho <ande1218@gmail.com>
2021-11-27 17:48:51 +0800

    [新增] 顏色選擇器面板與拉條上色完成

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,5 +1,5 @@
 import React from 'react'
-const ColorBoard = ({ setCoordinate, rgb }) => {
+const ColorBoard = ({ setCoordinate, rgb, barRGB }) => {
   const boardRef = React.useRef()
   const [start, setStart] = React.useState(null)
   const [position, setPosition] = React.useState({
@@ -39,17 +39,37 @@ const ColorBoard = ({ setCoordinate, rgb }) => {
       height={boardWidth}
       ref={boardRef}
     >
+      <linearGradient id="lg1" x1="100%" x2="0%">
+        <stop offset="0%" stopColor={`rgba(${barRGB.r}, ${barRGB.g}, ${barRGB.b}, 1)`} />
+        <stop offset="100%" stopColor="rgba(255, 255, 255, 1)" />
+      </linearGradient>
+      <linearGradient id="lg2" x1="100%" y1="100%" >
+        <stop offset="0%" stopColor="rgba(0, 0, 0 , 1)" />
+        <stop offset="100%" stopColor={`rgba(0, 0, 0, 0)`} />
+      </linearGradient>
       <rect
         strokeWidth="3"
         stroke="black"
         width={boardWidth - pointerRadius * 2}
-        height={boardWidth - pointerRadius * 2} x={pointerRadius} y={pointerRadius} fill={'#f5f5f5'}
+        height={boardWidth - pointerRadius * 2} x={pointerRadius} y={pointerRadius} fill="url(#lg1)"
       />
-      <circle cx={position.x} cy={position.y} r={pointerRadius} fill={`rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`} onMouseDown={startDrag} />
-    </svg>
+      <rect
+        strokeWidth="3"
+        stroke="black"
+        width={boardWidth - pointerRadius * 2}
+        height={boardWidth - pointerRadius * 2} x={pointerRadius} y={pointerRadius} fill="url(#lg2)"
+      />
+      <circle
+        strokeWidth="3"
+        stroke="black"
+        cx={position.x}
+        cy={position.y}
+        r={pointerRadius - 3} fill={`rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`} onMouseDown={startDrag}
+      />
+    </svg >
   )
 }
-const ColorBar = ({ setBarRatio, rgb }) => {
+const ColorBar = ({ setBarRatio, barRGB }) => {
   const boardRef = React.useRef()
   const [start, setStart] = React.useState(null)
   const [position, setPosition] = React.useState({
@@ -87,63 +107,82 @@ const ColorBar = ({ setBarRatio, rgb }) => {
       height={30}
       ref={boardRef}
     >
+      <defs>
+        <linearGradient id="lg3" x1="100%" x2="0%">
+          <stop offset="0%" stopColor="rgb(255, 0, 0)" />
+          <stop offset="16.6%" stopColor="rgb(255, 0, 255)" />
+          <stop offset="33.2%" stopColor="rgb(0, 0, 255)" />
+          <stop offset="49.8%" stopColor="rgb(0, 255, 255)" />
+          <stop offset="66.4%" stopColor="rgb(0, 255, 0)" />
+          <stop offset="83.0%" stopColor="rgb(255, 255, 0)" />
+          <stop offset="100%" stopColor="rgb(255, 0, 0)" />
+        </linearGradient>
+      </defs>
       <rect
         width={barWidth - pointerRadius * 2}
-        height={30} x={pointerRadius} y="0" fill={'#f5f5f5'}
+        height={30} x={pointerRadius} y="0" fill="url(#lg3)"
       />
-      <circle cx={position.x} cy={position.y} r={pointerRadius} fill={`rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`} onMouseDown={startDrag} />
+      <circle
+        strokeWidth="3"
+        stroke="black"
+        cx={position.x} cy={position.y} r={pointerRadius - 3} fill={`rgb(${barRGB.r}, ${barRGB.g}, ${barRGB.b})`} onMouseDown={startDrag} />
     </svg>
   )
 }
 const ColorPicker = () => {
   const [coordinate, setCoordinate] = React.useState({
     x: 100,
-    y: 100
+    y: 0
   })
   const [barRatio, setBarRatio] = React.useState(0)
+  const [barRGB, setBarRGB] = React.useState({
+    r: 255,
+    g: 0,
+    b: 0
+  })
   const [rgb, setRGB] = React.useState({
-    r: 0,
+    r: 255,
     g: 0,
     b: 0
   })
   React.useEffect(() => {
     if (barRatio >= 0 && 16.6 > barRatio) {
-      setRGB({
+      setBarRGB({
         r: 255,
         g: Math.floor(barRatio * 15.3),
         b: 0
       })
     } else if (barRatio >= 16.6 && 33.2 > barRatio) {
       let ratio = (barRatio - 16.6) / 16.6
-      setRGB({
+      setBarRGB({
         r: Math.floor(255 * (1 - ratio)),
         g: 255,
         b: 0
       })
     } else if (barRatio >= 33.2 && 49.8 > barRatio) {
       let ratio = (barRatio - 33.2) / 16.6
-      setRGB({
+      setBarRGB({
         r: 0,
         g: 255,
         b: Math.floor(255 * ratio)
       })
     } else if (barRatio >= 49.8 && 66.4 > barRatio) {
       let ratio = (barRatio - 49.8) / 16.6
-      setRGB({
+      setBarRGB({
         r: 0,
         g: Math.floor(255 * (1 - ratio)),
         b: 255
       })
     } else if (barRatio >= 66.4 && 83 > barRatio) {
       let ratio = (barRatio - 66.4) / 16.6
-      setRGB({
+      setBarRGB({
         r: Math.floor(255 * ratio),
         g: 0,
         b: 255
       })
     } else if (barRatio >= 83 && 100.1 > barRatio) {
       let ratio = (barRatio - 83) / 17
-      setRGB({
+      setBarRGB({
         r: 255,
         g: 0,
         b: Math.floor(255 * (1 - ratio)),
@@ -152,16 +191,25 @@ const ColorPicker = () => {
 
   }, [barRatio])
   React.useEffect(() => {
-    console.log(coordinate)
+    const xr = 255 - barRGB.r
+    const xg = 255 - barRGB.g
+    const xb = 255 - barRGB.b
+    const yr = barRGB.r + Math.floor(xr * (100 - coordinate.x) / 100)
+    const yg = barRGB.g + Math.floor(xg * (100 - coordinate.x) / 100)
+    const yb = barRGB.b + Math.floor(xb * (100 - coordinate.x) / 100)
+    setRGB({
+      r: Math.floor(yr * (100 - coordinate.y) / 100),
+      g: Math.floor(yg * (100 - coordinate.y) / 100),
+      b: Math.floor(yb * (100 - coordinate.y) / 100),
+    })
   }, [coordinate])
   return (
-    <div style={{ border: '1px solid', width: 400, height: 500 }}>
+    <div style={{ border: '1px solid', width: 400, height: 600 }}>
       <p>Color Picker</p>
       <p>{`r: ${rgb.r}, g: ${rgb.g}, b: ${rgb.b}`}</p>
-      <p>{`x: ${coordinate.x}, y: ${coordinate.y}`}</p>
-      <p>{`bar: ${barRatio}`}</p>
-      <ColorBoard setCoordinate={setCoordinate} rgb={rgb} />
-      <ColorBar setBarRatio={setBarRatio} rgb={rgb} />
+      <p>{`basic r: ${barRGB.r}, g: ${barRGB.g}, b: ${barRGB.b}`}</p>
+      <ColorBoard setCoordinate={setCoordinate} rgb={rgb} barRGB={barRGB} />
+      <ColorBar setBarRatio={setBarRatio} barRGB={barRGB} />
     </div>
   )
 }
